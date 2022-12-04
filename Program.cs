5a2f806347d176e1d9cfd16b30c8951b0dff9a1e
AnastasiaBuglakova <bugla2005@mail.ru>
2022-12-04 16:32:21 +0300

    InputArray Method added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string[] FilterArray(string[] array)
+{
+
+}
+
+
+
+string[] InputArray(int N)
+{
+    Console.WriteLine("Please, enter size of array:");
+    int N = Convert.ToInt32(Console.ReadLine());
+    string[] customArray = new string[N];
+    for(int i = 0; i < N; i++)
+    {
+        Console.WriteLine("Please, item # " + i + " of array:");
+        customArray[i] = Console.ReadLine();
+    }
+    return customArray;
+}
